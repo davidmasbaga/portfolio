3c24bb53a934d41ee6990e5aaeb529d375d1b43f
David Mas-Bagà <davidmasbaga@gmail.com>
2023-04-24 16:35:09 +0200

    url notion added

--- src/Components/Home/Projects.js
+++ src/Components/Home/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
 
     <div className={styles.body_projects_cards}>
     <ProjectCard tech='React' name="JobLink" image='https://res.cloudinary.com/dkxlwv844/image/upload/v1678808482/PortFolio/Joblink_ocvtyw.jpg' url='https://job-link.netlify.app/' gitHub="https://github.com/davidmasbaga/jobLink"/>
-    <ProjectCard tech='NextJs' name= "Notes to Notion" image="https://res.cloudinary.com/dkxlwv844/image/upload/v1682345100/Notes_to_Notion_en0znz.jpg" gitHub="https://github.com/davidmasbaga/notion_quick"/>
+    <ProjectCard tech='NextJs' name= "Notes to Notion" image="https://res.cloudinary.com/dkxlwv844/image/upload/v1682345100/Notes_to_Notion_en0znz.jpg" url="https://notion-quick-qj2j.vercel.app/" gitHub="https://github.com/davidmasbaga/notion_quick"/>
     <ProjectCard tech='Wordpress' name="TwoLittleDraws" image='https://res.cloudinary.com/dkxlwv844/image/upload/v1678809004/PortFolio/twolittledraws_etc3sm.jpg' url='https://twolittledraws.com/'/>
     </div>
     
